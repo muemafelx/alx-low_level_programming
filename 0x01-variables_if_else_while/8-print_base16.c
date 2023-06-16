76c8e7c4b47f2b13b079b2aef8c27d1a6a9dff40
muemafelx <muemafelix0@gmail.com>
2023-06-16 05:42:15 -0700

    Printing the numbers of base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Program entry point.
+ * Return: 0 if no error.
+ */
+int main(void)
+{
+	int i;
+	char hexvalues[] = "0123456789abcdef";
+
+	for (i = 0; i < 16; i++)
+	{
+		putchar(hexvalues[i]);
+	}
+	putchar('\n');
+	return (0);
+}
