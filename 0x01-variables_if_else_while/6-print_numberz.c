0b12cb79fb171bf4806078714c55f998fe8686c4
muemafelx <muemafelix0@gmail.com>
2023-06-16 05:29:18 -0700

    All single digit numbers of base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - Program entry point.
+ *Return:0 no error.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
